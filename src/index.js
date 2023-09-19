c28ba1832dbc89f61c3a15faa5a3f970c4801819
Machiiko <bagus.tito16@gmail.com>
2023-09-19 14:48:02 +0700

    moshpit new

---
+++ src/index.js
@@ -0,0 +1,125 @@
+const allHoverImages = document.querySelectorAll('.hover-container div img');
+const imgContainer = document.querySelector('.img-container');
+
+window.addEventListener('DOMContentLoaded', () => {
+    allHoverImages[0].parentElement.classList.add('active');
+});
+
+allHoverImages.forEach((image) => {
+    image.addEventListener('mouseover', () =>{
+        imgContainer.querySelector('img').src = image.src;
+        resetActiveImg();
+        image.parentElement.classList.add('active');
+    });
+});
+
+function resetActiveImg(){
+    allHoverImages.forEach((img) => {
+        img.parentElement.classList.remove('active');
+    });
+}
+
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", this.window.scrollY > 0);
+})
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
+
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+    
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+
+
+    // Zoom Function on Team Section
+    $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+      $(".zoom").css({
+          backgroundSize: (100 + scroll/250)  + "%",
+          top: -(scroll/10)  + "%",
+  
+      
+      });
+  }
+
+  );
+    
+
+    // Shrink the navbar 
+    navbarShrink();
+
+
+});
+
+// Quantity Button
+jQuery(document).ready(($) => {
+    $('.quantity').on('click', '.plus', function(e) {
+        let $input = $(this).prev('input.qty');
+        let val = parseInt($input.val());
+        $input.val( val+1 ).change();
+    });
+
+    $('.quantity').on('click', '.minus', 
+        function(e) {
+        let $input = $(this).next('input.qty');
+        var val = parseInt($input.val());
+        if (val > 0) {
+            $input.val( val-1 ).change();
+        } 
+    });
+});
+
+const slider = document.querySelector('.row-produk');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 1; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+      console.log(walk);
+    });
+
+    
